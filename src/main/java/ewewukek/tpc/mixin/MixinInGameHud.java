3c50dfe8712d231d809e0cfc7d3e67ccfe8dca85
ewewukek <ewewukek@gmail.com>
2021-02-22 04:37:52 +0300

    add indicator for max bow pull

--- src/main/java/ewewukek/tpc/mixin/MixinInGameHud.java
+++ src/main/java/ewewukek/tpc/mixin/MixinInGameHud.java
@@ -4,8 +4,14 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.options.Perspective;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.BowItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 
 @Mixin(InGameHud.class)
 public class MixinInGameHud {
@@ -15,5 +21,37 @@ public class MixinInGameHud {
     )
     private boolean doRenderCrosshair(Perspective perspective) {
         return !perspective.isFrontView();
+    }
+
+    @Redirect(
+        method = "renderCrosshair(Lnet/minecraft/client/util/math/MatrixStack;)V",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V")
+    )
+    private void drawTexture(InGameHud hud, MatrixStack matrices, int dst_x, int dst_y, int src_x, int src_y, int w, int h) {
+        if (src_y == 94) {
+            dst_x -= 1; // fix indicator position
+        }
+        hud.drawTexture(matrices, dst_x, dst_y, src_x, src_y, w, h);
+
+        if (src_x == 0 && src_y == 0) { // main crosshair
+            InGameHudAccessor acc = (InGameHudAccessor)hud;
+            boolean bowReady = false;
+
+            MinecraftClient client = acc.getClient();
+            ClientPlayerEntity player = client.player;
+            ItemStack itemStack = player.getActiveItem();
+            if (player.isUsingItem() && itemStack.getItem() == Items.BOW) {
+                int ticksInUse = Items.BOW.getMaxUseTime(itemStack) - player.getItemUseTimeLeft();
+                if (BowItem.getPullProgress(ticksInUse) == 1.0F ) {
+                    bowReady = true;
+                }
+            }
+
+            if (bowReady) { // small tick under main crosshair
+                int k = acc.getScaledWidth() / 2 - 8 + 6;
+                int j = acc.getScaledHeight() / 2 - 7 + 16 + 4;
+                hud.drawTexture(matrices, k, j, 75, 98, 3, 3);
+            }
+        }
     }
 }
