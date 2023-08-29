0b8faa47b226aa30d58a019cafbecda5306c440a
ewewukek <ewewukek@gmail.com>
2023-08-30 01:51:07 +0300

    update to 1.20.1

--- src/main/java/ewewukek/tpc/mixin/MixinInGameHud.java
+++ src/main/java/ewewukek/tpc/mixin/MixinInGameHud.java
@@ -6,19 +6,20 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 import ewewukek.tpc.Config;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.option.Perspective;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.BowItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.TridentItem;
+import net.minecraft.util.Identifier;
 
 @Mixin(InGameHud.class)
 public class MixinInGameHud {
     @Redirect(
-        method = "renderCrosshair(Lnet/minecraft/client/util/math/MatrixStack;)V",
+        method = "renderCrosshair(Lnet/minecraft/client/gui/DrawContext;)V",
         at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/Perspective;isFirstPerson()Z")
     )
     private boolean doRenderCrosshair(Perspective perspective) {
@@ -34,16 +35,16 @@ public class MixinInGameHud {
     }
 
     @Redirect(
-        method = "renderCrosshair(Lnet/minecraft/client/util/math/MatrixStack;)V",
-        at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V")
+        method = "renderCrosshair(Lnet/minecraft/client/gui/DrawContext;)V",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V")
     )
-    private void drawTexture(MatrixStack matrices, int dst_x, int dst_y, int src_x, int src_y, int w, int h) {
+    private void drawTexture(DrawContext context, Identifier textureId, int dst_x, int dst_y, int src_x, int src_y, int w, int h) {
         InGameHud hud = (InGameHud)(Object)this;
 
         if (src_y == 94) {
             dst_x -= 1; // fix indicator position
         }
-        InGameHud.drawTexture(matrices, dst_x, dst_y, src_x, src_y, w, h);
+        context.drawTexture(textureId, dst_x, dst_y, src_x, src_y, w, h);
 
         if (src_x == 0 && src_y == 0) { // main crosshair
             boolean weaponReady = false;
@@ -69,7 +70,7 @@ public class MixinInGameHud {
             if (weaponReady) { // small tick under main crosshair
                 int k = hud.scaledWidth / 2 - 2;
                 int j = hud.scaledHeight / 2 + 6;
-                InGameHud.drawTexture(matrices, k, j, 75, 98, 3, 3);
+                context.drawTexture(textureId, k, j, 75, 98, 3, 3);
             }
         }
     }
